f87c156414f3728b91fa1b78ebe31032e61a1525
Shivani21-hude <shivani21hude@gmail.com>
2023-02-05 21:16:54 +0530

    [Shivani]-Add person list as ssn,name,address and age

---
+++ LambdaExpressionDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LambdaExpressionDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> listPersonInCity = new List<Person>();
+            LambdaDemo.AddRecords(listPersonInCity);
+           
+            Console.ReadKey();
+        }
+    }
+}
